ff195beb6fbe47247acac6899bd2ef58f485a287
Dante Besfalin? <befalinfreelancer@gmail.com>
2026-05-24 14:35:26 +0000

    топ дичь

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-	  HELLO
+    <Header />
+    <Footer />
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            2077 besfalin.ru все а.п защины роскомпозором
+        </footer>
+    )
+}
+
+export default Footer
