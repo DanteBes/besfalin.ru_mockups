89dbc08b11b2e05b4af703b674047a2dc1bd2aae
Dante Besfalin? <befalinfreelancer@gmail.com>
2026-05-25 15:24:28 +0000

    обнулил цсс

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
+import styles from "./Footer.module.css"
+
 function Footer() {
     return (
         <footer>
-            2077 besfalin.ru все а.п защины роскомпозором
+            2077 
+            <a className={styles.link}>
+             free vpn only now
+            </a>
+            все а.п защины роскомпозором
         </footer>
     )
 }
