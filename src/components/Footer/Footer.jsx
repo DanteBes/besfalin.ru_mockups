60d26991315b80c3c172629dd27ac4ce725d9072
Dante Besfalin? <befalinfreelancer@gmail.com>
2026-05-25 15:53:05 +0000

    убрал контент в подвале и добавил в футтер

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,7 @@ import styles from "./Footer.module.css"
 function Footer() {
     return (
         <footer>
-            2077 
-            <a className={styles.link}>
-             free vpn only now
-            </a>
-            все а.п защины роскомпозором
+            
         </footer>
     )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,33 @@
 function Header() {
     return (
         <header>
-            Я тут был?
+            <div>
+                <img rcs=""/>
+                <div>
+                    <h1>Yaroslav Besfaln</h1>
+                    <span>veb-developer</span>
+                </div>
+
+                <div>
+                    <ol>
+                        <li>
+                            <a>
+                                about
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                contacts
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                projects
+                            </a>
+                        </li>
+                    </ol>
+                </div>
+            </div>
         </header>
     )
 }
