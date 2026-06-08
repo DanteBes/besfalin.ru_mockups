31bff884e502016c500dc01f5dea73a8fef87bda
Dante Besfalin? <befalinfreelancer@gmail.com>
2026-06-08 13:14:05 +0000

    отредачил секцию блок, добавил пару стилей для Blog.jsx и добаботал майн

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -4,23 +4,25 @@ function Blog() {
     return (
         <section className={styles.blogSection}>
             <div className={styles.blogContainer}>
-                <div className={styles.blogHeader}>
-                    <h1 className={styles.blogTitle}>
-                        Мой блог
-                    </h1>
-                </div>
+	    	<div className={styles.blogLeftSide}>
+                    <div className={styles.blogHeader}>
+                        <h1 className={styles.blogTitle}>
+                            Мой блог
+                        </h1>
+                    </div>
 
-                <div className={styles.blogList}>
-                    <h2 className={styles.blogItem}>
-                        01 Как я начал кодить
-                    </h2>
-                    <h2 className={styles.blogItem}>
-                        02 Сломался планшет
-                    </h2>
-                    <h2 className={styles.blogItem}>
-                        03 Мне выпал ЛЕОН!!!
-                    </h2>
-                </div>
+                    <div className={styles.blogList}>
+                        <h2 className={styles.blogItem}>
+                            01 Как я начал кодить
+                        </h2>
+                        <h2 className={styles.blogItem}>
+                            02 Сломался планшет
+                        </h2>
+                        <h2 className={styles.blogItem}>
+                            03 Мне выпал ЛЕОН!!!
+                        </h2>
+                    </div>
+	    	</div>
 
                 <div className={styles.blogFooter}>
                     <p className={styles.blogDescription}>
