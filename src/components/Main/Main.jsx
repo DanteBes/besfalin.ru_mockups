6ab7cd99dc023d0ee4d34b1ad83ee4d8c4391a10
Dante Besfalin? <befalinfreelancer@gmail.com>
2026-06-14 15:42:54 +0000

    наладил стили в main section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,34 +4,36 @@ import ava from "../../assets/ava.png"
 function Main() {
     return (
         <main className={styles.previewMain}>
-             <div className={styles.previewLogo}>
-                <h2 className={styles.previewSubtitle}>Всем привет это сайт,</h2>
-                <h1 className={styles.previewTitle}>Ярослава Бесфалина</h1>
-                <p className={styles.previewParagraph}>
-                    Разработчик, энтузиаст, исследователь технологий.Делаю веб-проекты и делюсь опытом в блоге.
-                </p>
+            <div className={styles.previewMainContent}>
+		    <div className={styles.previewLogo}>
+			<h2 className={styles.previewSubtitle}>Всем привет<br/>это сайт,</h2>
+			<h1 className={styles.previewTitle}>Ярослава Бесфалина</h1>
+			<p className={styles.previewParagraph}>
+			    Разработчик, энтузиаст, исследователь технологий. Делаю веб-проекты и делюсь опытом в блоге.
+			</p>
 
-                <ol className={styles.previewMainList}>
-                    <li className={styles.previewListObject}>
-                        <a className={styles.previewLink}>
-                            🏙️ VK
-                        </a>
-                    </li>
-                    <li className={styles.previewListObject}>
-                        <a className={styles.previewLink}>
-                            🏙️ TG
-                        </a>
-                    </li>
-                    <li className={styles.previewListObject}>
-                        <a className={styles.previewLink}>
-                            🏙️ YT
-                        </a>
-                    </li>
-                </ol>
-             </div>
+			<ol className={styles.previewMainList}>
+			    <li className={styles.previewListObject}>
+				<a className={styles.previewLink}>
+				    🏙️ VK
+				</a>
+			    </li>
+			    <li className={styles.previewListObject}>
+				<a className={styles.previewLink}>
+				    🏙️ TG
+				</a>
+			    </li>
+			    <li className={styles.previewListObject}>
+				<a className={styles.previewLink}>
+				    🏙️ YT
+				</a>
+			    </li>
+			</ol>
+		     </div>
 
-             <div className={styles.objF}>
-                <img src={ava} alt="А где картинка?"/>
+		     <div className={styles.objF}>
+			<img className={styles.ava} src={ava} alt="А где картинка?"/>
+		     </div>
              </div>
         </main>
     )
