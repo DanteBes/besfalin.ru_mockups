30128b598f6388958e70a92119d0b585ef8c8d22
Dante Besfalin? <befalinfreelancer@gmail.com>
2026-06-18 16:07:44 +0000

    убрал 'br' в майн и подправил стили в майн

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,7 +11,7 @@ function Main() {
         <main className={styles.previewMain}>
             <div className={styles.previewMainContent}>
 		    <div className={styles.previewLogo}>
-			<h2 className={styles.previewSubtitle}>Всем привет<br/>это сайт,</h2>
+			<h2 className={styles.previewSubtitle}>Всем привет это сайт,</h2>
 			<h1 className={styles.previewTitle}>Ярослава Бесфалина</h1>
 			<p className={styles.previewParagraph}>
 			    Разработчик, энтузиаст, исследователь технологий. Делаю веб-проекты и делюсь опытом в блоге.
