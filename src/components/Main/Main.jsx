56597c847d5b45d74ec3b7a27ec53c06b937cb48
Dante Besfalin? <befalinfreelancer@gmail.com>
2026-06-19 15:34:34 +0000

    исправил зоголовки в секции 'main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,8 +11,8 @@ function Main() {
         <main className={styles.previewMain}>
             <div className={styles.previewMainContent}>
 		    <div className={styles.previewLogo}>
-			<h2 className={styles.previewSubtitle}>Всем привет это сайт,</h2>
-			<h1 className={styles.previewTitle}>Ярослава Бесфалина</h1>
+			<h2 className={styles.previewSubtitle}>Всем привет с вами</h2>
+			<h1 className={styles.previewTitle}>Ярослав Бесфалин</h1>
 			<p className={styles.previewParagraph}>
 			    Разработчик, энтузиаст, исследователь технологий. Делаю веб-проекты и делюсь опытом в блоге.
 			</p>
