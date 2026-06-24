1757089685016f7e18ae14b75045c4512bd063e3
Dante Besfalin? <befalinfreelancer@gmail.com>
2026-06-24 16:44:58 +0000

    Поготовил футер к дальнейшей работе⚒️

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,23 @@
 import styles from './Footer.module.css'
+import { FaGithub } from 'react-icons/fa'
 
 function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <span className={styles.footerText}>© 2026 Dante Besfalin</span>
+        <ul className={styles.FooterList}>
+          <li className={styles.FooterObject}>
+            <a className={styles.FooterLink}>
+              <FaGithub />
+            </a>
+          </li>
+          <li className={styles.FooterObject}>
+            <a className={styles.FooterLinkFrend}>
+              <FaGithub />
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   )
