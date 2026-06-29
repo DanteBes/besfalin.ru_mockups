0b50c9d1d11ff1bc5c15ec1670b650aae53f5d26
Dante Besfalin? <befalinfreelancer@gmail.com>
2026-06-29 15:58:10 +0000

    Добавил в 'About.jsx' тэг img, time, span и классы и после стилизовал⚒️

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,15 @@
 import styles from './About.module.css'
+import ava from '../../assets/ava.png'
 
 function About() {
   return (
     <main className={styles.about}>
       <div className={styles.aboutContent}>
-        <p className={styles.aboutText}>Обо мне текст</p>
+        <img className={styles.aboutAva} src={ava} alt="А где картинка?" />
+        <span className={styles.aboutAttendance}>
+          Был в сети:
+          <time dateTime="2077-07-17 02:13">2077-07-10</time>
+        </span>
       </div>
     </main>
   )
