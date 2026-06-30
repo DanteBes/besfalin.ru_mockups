b6f517f6b028349933ec45a7b5ef03b752e6dc51
Dante Besfalin? <befalinfreelancer@gmail.com>
2026-06-30 13:29:43 +0000

    добавил карточки и тестовую кнопку для открытия и скрытия параграфа под заголовком. при поддержке - (© art0tod)

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,14 @@
 import styles from './About.module.css'
 import ava from '../../assets/ava.png'
+import { useState } from 'react'
 
 function About() {
+  const [isActive, setIsActive] = useState(true)
+
+  const handleToggle = () => {
+    setIsActive(!isActive)
+  }
+
   return (
     <main className={styles.about}>
       <div className={styles.aboutContent}>
@@ -11,6 +18,37 @@ function About() {
             Был в сети:
             <time dateTime="2077-07-17 02:13">2077-07-10</time>
           </span>
+        </div>
+        <div className={styles.textWrapper}>
+          <div
+            className={isActive ? styles.textBlock : styles.textBlock + ' ' + styles.textBlockLow}
+          >
+            <div className={styles.titleWrapper}>
+              <h1 className={styles.title}>title</h1>
+              <span onClick={handleToggle}>тест-кнопка</span>
+            </div>
+            <p className={styles.paragraph}>
+              lorem ipsum dolor sit amet, consectetur adipiscing elit. aliquam lectus risus, finibus
+              ornare vestibulum et, feugiat quis dui. vivamus sit amet dolor et magna facilisis
+              rhoncus. curabitur maximus est sed porta scelerisque. sed suscipit, arcu volutpat
+              feugiat posuere, eros nisi tristique nibh, mollis vehicula lectus tortor eu purus.
+              donec ut tortor blandit, sagittis diam eget, suscipit eros. quisque at magna neque.
+              nulla faucibus mi at nunc mattis placerat.
+            </p>
+          </div>
+          <div className={styles.textBlock}>
+            <div className={styles.titleWrapper}>
+              <h1 className={styles.title}>title</h1>
+            </div>
+            <p className={styles.paragraph}>
+              lorem ipsum dolor sit amet, consectetur adipiscing elit. aliquam lectus risus, finibus
+              ornare vestibulum et, feugiat quis dui. vivamus sit amet dolor et magna facilisis
+              rhoncus. curabitur maximus est sed porta scelerisque. sed suscipit, arcu volutpat
+              feugiat posuere, eros nisi tristique nibh, mollis vehicula lectus tortor eu purus.
+              donec ut tortor blandit, sagittis diam eget, suscipit eros. quisque at magna neque.
+              nulla faucibus mi at nunc mattis placerat.
+            </p>
+          </div>
         </div>
       </div>
     </main>
